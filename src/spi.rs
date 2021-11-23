e4aa1c2c747e754b652fb914cb58362943fc314c
Matt Keeter <matt@oxide.computer>
2021-11-23 14:43:19 -0500

    cargo fmt

--- src/spi.rs
+++ src/spi.rs
@@ -8,7 +8,7 @@ use anyhow::{bail, Result};
 /// hint to disambiguate).
 pub fn spi_task(
     hubris: &HubrisArchive,
-    peripheral: Option<u8>
+    peripheral: Option<u8>,
 ) -> Result<HubrisTask> {
     let lookup = |peripheral| {
         let spi = format!("spi{}", peripheral);
